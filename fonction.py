24445d0682efb48c49a849cdceb43360a252a0b7
bienfait musoro <bienfaitmusoto@gmail.com>
2025-04-21 05:13:02 -0700

    fonctionpy

---
+++ fonction.py
@@ -0,0 +1,9 @@
+"""
+def afficher_message():
+    print("Bonjour, comment ca va?")
+afficher_message()"""
+
+def afficher_message(nom,premom):
+   print("nom:",nom)
+   print("Prenom:",premom)
+afficher_message("dupont","jean")
