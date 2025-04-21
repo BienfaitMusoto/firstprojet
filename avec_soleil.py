e6c6d1f520f1e7ed6a750b4d18ad748638498abb
bienfait musoro <bienfaitmusoto@gmail.com>
2025-04-21 05:18:19 -0700

    avec_soleilpy

---
+++ avec_soleil.py
@@ -0,0 +1,9 @@
+avec_soleil= True
+en_semaine =  False
+
+if avec_soleil and not en_semaine :
+    print("on va a la page!")
+elif neige:
+    print("on va au travail")
+else:
+    print("on reste a la maison!")
