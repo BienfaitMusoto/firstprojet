ec076567c777eb2236f4c9c8ad1032ed7ef63cbb
bienfait musoro <bienfaitmusoto@gmail.com>
2025-04-21 00:01:47 -0700

    exercice3py

---
+++ exercice3.py
@@ -0,0 +1,9 @@
+nom="jean"
+age=23
+taille=4,2
+est_etudiant= True  
+print(f"je m'appele {nom},j'ai {age}, j'ai {taille} et je suis etudiant {est_etudiant}")
+print(type(nom))
+print(type(age))
+print(type(taille))
+print(type(est_etudiant))
