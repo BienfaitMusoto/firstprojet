0106cdf2e6fe1560c12bb4b6bcd65da5bc2a4476
bienfait musoro <bienfaitmusoto@gmail.com>
2025-04-21 05:30:06 -0700

    bouclepy

---
+++ boucle.py
@@ -0,0 +1,29 @@
+"""
+races_de_chien =["golden retriever","chihuahua","terrier","carlin"]
+for chien in races_de_chien:
+    print(chien)
+"""
+#pour le variable
+"""
+nom = "ordinateur"
+for ordi in nom:
+    print(ordi)
+"""
+# pour le range
+"""for x in range(5)
+ print(f"{x}bouteilles de bieres au mur!")
+ """
+ # pour while
+"""
+capacite_maximale = 10
+capacite_actuelle = 3
+while capacite_actuelle < capacite_maximale:
+    print(f"la capacite actuelle est {capacite_actuelle} ")
+    capacite_actuelle += 1 """
+
+liste =[1,2,3,4,5]
+ # boucle for sur la liste
+for element in liste:
+    if element ==3:
+        continue
+    print(element)
