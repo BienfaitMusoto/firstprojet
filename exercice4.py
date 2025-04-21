ee9da582ad9e0d7986c7bfd81c0ad3c0b045ee4a
bienfait musoro <bienfaitmusoto@gmail.com>
2025-04-21 02:31:50 -0700

    exercice4py

---
+++ exercice4.py
@@ -0,0 +1,16 @@
+fruit=["pomme","banane","orange"]
+fruit.append("kiwi")
+fruit.remove("orange")
+fruit[1]
+print("longueur de liste fruit :", len(fruit))
+fruit.sort()
+print("liste triee des fruit:",fruit)
+
+
+
+nouvelle_campagne={
+    "responsable_de_campagne":"jeanne d'arc"
+    "nom_de_campagne":"campagne nous aimons les chiens"
+    "date_date_debut":"01/01/2026"
+    "influenceurs_importants":[@MonAmour]
+}
