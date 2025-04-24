aea061ec8f63803966be7f0269fc7984eaa61560
bienfait musoro <bienfaitmusoto@gmail.com>
2025-04-24 10:09:49 -0700

    drinkpy

---
+++ drink.py
@@ -0,0 +1,14 @@
+class Drink:
+    def __init__(self,price):
+        self.price = price
+    def drink(self):
+        print("je ne sais pas ce que c'est, mais je le bois.")
+class coffee(Drink):
+    price = {"simple":1,"serre":1,"allonge":1.5}
+    def __init__(self,type):
+        self.type = type
+        super().__init__(price = self.price.get(type, 1))
+    def drink(self):
+        print(f"un bon cafe de {self.type} qui coute {self.price}$ pour me reveiller !")
+coffee = coffee("allonge")
+boire = coffee.drink()
