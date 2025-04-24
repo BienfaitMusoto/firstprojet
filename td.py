d5daf19f7fa8e0acd33a38ce39ba5c0069c0ebd3
bienfait musoro <bienfaitmusoto@gmail.com>
2025-04-24 10:27:38 -0700

    tdpy

---
+++ td.py
@@ -0,0 +1,28 @@
+class marteaux:
+    def __init__(self,Type_de_marteaux,Taille, couleur):
+        self.Type_de_marteaux = Type_de_marteaux
+        self.Taille = Taille 
+        self.couleur = couleur
+
+    def Plante_clous(self):
+      pass
+    def Retirer_clous(self):
+      pass
+    def Modifier_clous(self):
+     pass
+
+class tournevis:
+    def __init__(self,marque,Taille):
+       self.marque = marque
+       self.Taille = Taille 
+
+    def Serrer_vis(self):
+        pass
+    def Desserrer_vis(self):
+       pass
+
+
+
+
+
+    
